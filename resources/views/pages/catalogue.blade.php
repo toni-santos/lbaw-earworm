d424afeb96330948b5e21e130db7960f192547ac
toni-santos <a.oliveirasantos122@gmail.com>
2022-12-21 19:28:53 +0000

    default catalog page front end fixes

--- resources/views/pages/catalogue.blade.php
+++ resources/views/pages/catalogue.blade.php
@@ -13,13 +13,13 @@
                     <option value="desc-rating">Descending Rating</option>
                 </select>
                 <!-- filters: genre artist year price rating -->
-                <p>Genre</p>
+                <p class="filter-title">Genre</p>
                 <div class="scroll-filter" id="fiter-genre">
                     @foreach ($genres as $genre)
                     <label><input type="checkbox" class="checkbox"><span class="genre-check">{{$genre->name}}</span></label>
                     @endforeach
                 </div>
-                <p>Year</p>
+                <p class="filter-title">Year</p>
                 <div class="scroll-filter" id="filter-year">
                     <label><input type="checkbox" class="checkbox"><span class="year-check">YEAR</span></label>
                     <label><input type="checkbox" class="checkbox"><span class="year-check">YEAR</span></label>
@@ -31,7 +31,7 @@
                     <label><input type="checkbox" class="checkbox"><span class="year-check">YEAR</span></label>
                     <label><input type="checkbox" class="checkbox"><span class="year-check">YEAR</span></label>
                 </div>
-                <p>Artist</p>
+                <p class="filter-title">Artist</p>
                 <div class="scroll-filter" id="filter-artist">
                     <label><input type="checkbox" class="checkbox"><span class="artist-check">CD</span></label>
                     <label><input type="checkbox" class="checkbox"><span class="artist-check">Vinyl</span></label>
@@ -39,14 +39,14 @@
                     <label><input type="checkbox" class="checkbox"><span class="artist-check">DVD</span></label>
                     <label><input type="checkbox" class="checkbox"><span class="artist-check">Box Set</span></label>
                 </div>
-                <p>Price</p>
+                <p class="filter-title">Price</p>
                 <div class="box-filter" id="filter-price">
                     <label for="min-price">Min Price</label>
                     <input id="min-price" name="min-price" type="number" min="1" step="any">
                     <label for="max-price">Max Price</label>
                     <input id="max-price" name="max-price" type="number" min="1" step="any">
                 </div>
-                <p>Rating</p>
+                <p class="filter-title">Rating</p>
                 <div class="box-filter" id="filter-rating">
                     <label for="min-rating">Min Rating</label>
                     <input id="min-rating" name="min-rating" type="number" min="1" step="any">
@@ -54,17 +54,20 @@
                     <input id="max-rating" name="max-rating" type="number" min="1" step="any">
                 </div>
             </fieldset>
+            <button type="submit" class="confirm-button">FILTER</button>
         </form>
     </section>
-    <section id="results">
-        @if (!empty($products)) 
-            @foreach ($products as $product)
-                @include('partials.common.product-card', ['product' => $product])
-            @endforeach
-        @else
-            <p>No products correspond to your search!</p>
-        @endif
-    </section>
-    {{ $products->links('vendor.pagination.default') }}
+    <div class="results-wrapper">
+        <section id="results">
+            @if (!empty($products)) 
+                @foreach ($products as $product)
+                    @include('partials.common.product-card', ['product' => $product])
+                @endforeach
+            @else
+                <p>No products correspond to your search!</p>
+            @endif
+        </section>
+        {{ $products->links('vendor.pagination.default') }}
+    </div>
 </main>
 @include('partials.common.foot')
--- resources/views/partials/common/product-card.blade.php
+++ resources/views/partials/common/product-card.blade.php
@@ -1,7 +1,7 @@
 <article class="product-card">
     <a href="/product/{{$product['id']}}"><img class="product-card-img" src={{ url('/images/products/'.$product['id'].'.jpg') }}></a>
     <article class="product-desc">
-        <a href="/product/{{$product['id']}}" class="prod-name" title="Product Name">{{$product['name']}}</a>
+        <a href="/product/{{$product['id']}}" class="prod-name" title="{{$product['name']}}">{{$product['name']}}</a>
         <a href="/artists/{{$product['artist_id']}}" class="prod-artist-name" title="Artist Name">{{$product['artist_name']}}</a>
         <article class="product-specs">
             <a class="prod-format" title="Format">{{$product['format']}}</a>
