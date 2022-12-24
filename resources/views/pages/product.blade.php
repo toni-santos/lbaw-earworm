240164c8b29984f7ec7d67e956b346c8aaff354b
toni-santos <a.oliveirasantos122@gmail.com>
2022-12-24 16:14:24 +0000

    fixed product wishlist button

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -27,15 +27,16 @@
             <form id="buy-form" action="{{route('buyProduct', ['id' => $product->id])}}">
                 <button type=submit class="confirm-button">BUY</button>
             </form>
-                @if (in_array($product['id'], $wishlist))
-                    <p class="wishlist-container" id="favorite-container-{{$product->id}}" onclick="toggleLike(event, {{$product['id']}})">Wishlisted<span class="material-icons fav-album">favorite</span>
-                @else
-                    <p class="wishlist-container" id="favorite-container-{{$product->id}}" onclick="toggleLike(event, {{$product['id']}})">Add to Wishlist<span class="material-icons fav-album">favorite_outline</span>
-                @endif
-                    </p>
             @else
             <button class="confirm-button" href="{{route('buyProduct', ['id' => $product->id])}}" disabled>BUY</button>
             @endif
+            @if (in_array($product['id'], $wishlist))
+                <p class="wishlist-container" id="favorite-container-{{$product->id}}" onclick="toggleLike(event, {{$product['id']}})">Wishlisted<span class="material-icons fav-album">favorite</span>
+            @else
+                <p class="wishlist-container" id="favorite-container-{{$product->id}}" onclick="toggleLike(event, {{$product['id']}})">Add to Wishlist<span class="material-icons fav-album">favorite_outline</span>
+            @endif
+                </p>
+
         </div>
     </div>
     <div id="product-tracklist-wrapper">
