eb882c4d4a7a11870fbed205a9a2fc2b865bd1e8
JosA(c) Maria Borges Pires do Couto e Castro <up202006963@edu.fe.up.pt>
2022-12-28 21:27:09 +0000

    css fix

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -6,19 +6,19 @@
         <section class="inputs-box">
             <div class="input-container">
                 <input class="text-input" type="email" name="email" autocomplete="email" placeholder=" " onkeyup="updateForm(event)" onfocus="checkFilled(event)" required>
-                <label class="input-label" for="email" onclick="setFocus(event)">Email</label>
+                <label class="input-label" for="email" onclick="setFocus(event)">Account Email</label>
                 <span class="required-alert">Required</span>
             </div>
             <div class="input-container">
                 <input class="text-input" id="password-input" type="password" name="password" placeholder=" " autocomplete="current-password" minlength="8" onkeyup="updateForm(event); updateCounter(event)" onkeydown="updateCounter(event)" onfocus="checkFilled(event)" required>
-                <label class="input-label" for="pwd" onclick="setFocus(event)">Password</label>
+                <label class="input-label" for="pwd" onclick="setFocus(event)">New Password</label>
                 <span class="material-icons" id="password-eye" onclick="showPassword(event)">visibility</span>
                 <span id="password-cnt">0/8</span>
                 <span class="required-alert">Required</span>
             </div>
             <div class="input-container">
                 <input class="text-input" id="confirm-password-input" type="password" name="password_confirmation" placeholder=" " autocomplete="current-password" minlength="8" onkeyup="updateForm(event); updateCounter(event)" onkeydown="updateCounter(event)" onfocus="checkFilled(event)" required>
-                <label class="input-label" for="password_confirmation" onclick="setFocus(event)">Password</label>
+                <label class="input-label" for="password_confirmation" onclick="setFocus(event)">Confirm Password</label>
                 <span class="material-icons" id="password-eye" onclick="showPassword(event)">visibility</span>
                 <span id="password-cnt">0/8</span>
                 <span class="required-alert">Required</span>
